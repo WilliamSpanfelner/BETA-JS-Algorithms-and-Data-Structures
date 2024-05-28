4e0f8830a658066255e11984f7a094d5e6bd1206
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-28 15:49:53 +0300

    Add empty template literal to innerHTML property of dessertCards

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -86,4 +86,6 @@ const products = [
         category: "Cupcake",
     },
 ];
-products.forEach(({name, id, price, category}) => {});
+products.forEach(({name, id, price, category}) => {
+    dessertCards.innerHTML += ``;
+});
