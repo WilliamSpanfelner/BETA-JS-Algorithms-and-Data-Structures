3613e93f8a2ca92226d9706411816cc2bd10f8f2
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-29 10:05:10 +0300

    Create var to get the total for items by product.id

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -116,5 +116,6 @@ class ShoppingCart {
         this.items.forEach((dessert) => {
             totalCountPerProduct[dessert.id] = (totalCountPerProduct[dessert.id] || 0) + 1;
         });
+        const currentProductCount = totalCountPerProduct[product.id];
     }
 };
