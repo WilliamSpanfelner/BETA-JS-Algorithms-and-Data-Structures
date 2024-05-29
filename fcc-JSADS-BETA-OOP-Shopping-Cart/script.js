8e876855582a386f97ff13968988abd3d91e1c9a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-29 15:19:51 +0300

    Update the HTML with the subtotal value

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -133,6 +133,7 @@ class ShoppingCart {
         const subTotal = this.items.reduce((total, item) => total + item.price, 0);
         const tax = this.calculateTaxes(subTotal);
         this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
     }
     calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
