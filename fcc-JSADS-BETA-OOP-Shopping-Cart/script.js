2c25a347ff8449314296c8cc91b6d120e8f3a736
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-29 09:07:02 +0300

    Track the number of each item in basket

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -111,5 +111,7 @@ class ShoppingCart {
         const product = products.find((item) => item.id === id);
         const {name , price} = product;
         this.items.push(product);
+
+        const totalCountPerProduct = {};
     }
 };
