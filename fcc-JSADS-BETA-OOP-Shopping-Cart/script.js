ce81a2ceb3146456d8a4f7ec0839b0d528fcc1ec
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-30 14:35:31 +0300

    Reset productsContainer

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -150,6 +150,8 @@ class ShoppingCart {
         if (isCartCleared) {
             this.items = [];
             this.total = 0;
+            productsContainer.innerHTML = '';
+
         }
     }
 };
