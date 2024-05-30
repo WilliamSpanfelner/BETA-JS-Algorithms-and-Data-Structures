773c022975030604dc7ecc71258e5f276835fc72
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-30 13:55:56 +0300

    Add clearCart method to ShoppingCart class

--- fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
+++ fcc-JSADS-BETA-OOP-Shopping-Cart/script.js
@@ -141,6 +141,7 @@ class ShoppingCart {
     calculateTaxes(amount) {
         return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
     }
+    clearCart() {}
 };
 
 const cart = new ShoppingCart();
