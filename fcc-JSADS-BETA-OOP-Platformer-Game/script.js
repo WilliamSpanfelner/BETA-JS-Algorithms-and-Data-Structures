7c9b72ed273824dff07edc5eb656ec1483da6b57
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-05-31 15:16:41 +0300

    Kill velocity.y outside canvas bounds

--- fcc-JSADS-BETA-OOP-Platformer-Game/script.js
+++ fcc-JSADS-BETA-OOP-Platformer-Game/script.js
@@ -45,6 +45,8 @@ class Player {
                 this.velocity.y = gravity;
             }
             this.velocity.y += gravity;
+        } else {
+            this.velocity.y = 0;
         }
     };
 }
