32c395de6ab8d1031ebdd0dd53a86ea8fb85996b
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-01 10:04:41 +0300

    Set player's velocity and return

--- fcc-JSADS-BETA-OOP-Platformer-Game/script.js
+++ fcc-JSADS-BETA-OOP-Platformer-Game/script.js
@@ -86,6 +86,8 @@ startBtn.addEventListener("click", startGame);
 
 const movePlayer = (key, xVelocity, isPressed) => {
     if (!isCheckpointCollisionDetectionActive) {
-        
+        player.velocity.x = 0;
+        player.velocity.y = 0;
+        return;
     }
 };
