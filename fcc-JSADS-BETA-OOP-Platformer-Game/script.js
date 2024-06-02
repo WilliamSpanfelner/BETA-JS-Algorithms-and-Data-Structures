7ea7a5329b00eefc0324b1530548d644c3ccfcd5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-02 17:25:44 +0300

    Add conditional to check rules array

--- fcc-JSADS-BETA-OOP-Platformer-Game/script.js
+++ fcc-JSADS-BETA-OOP-Platformer-Game/script.js
@@ -169,6 +169,7 @@ const animate = () => {
             player.position.x - player.width <= checkpoint.position.x - checkpoint.width + player.width * 0.9,
             index === 0 || checkpoints[index - 1].claimed === true,
         ];
+        if (checkpointDetectionRules.every((el) => el)) {};
     });
 };
 
