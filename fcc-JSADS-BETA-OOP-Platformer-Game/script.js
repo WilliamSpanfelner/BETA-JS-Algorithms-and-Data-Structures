937f2daf3bd022d016109676f457a185ef6fb7c5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-02 17:34:37 +0300

    Perform actions as specified

--- fcc-JSADS-BETA-OOP-Platformer-Game/script.js
+++ fcc-JSADS-BETA-OOP-Platformer-Game/script.js
@@ -171,8 +171,10 @@ const animate = () => {
         ];
         if (checkpointDetectionRules.every((el) => el)) {
             checkpoint.claim();
-            if (index == checkpoints.length - 1) {
-                
+            if (index === checkpoints.length - 1) { 
+                isCheckpointCollisionDetectionActive = false;
+                showCheckpointScreen("You reached the final checkpoint!");
+                movePlayer("ArrowRight", 0, false);
             }
         };
     });
