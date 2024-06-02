49eeed7c4d5e646baa8665d3b72ade93e8697cc5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-02 16:46:26 +0300

    Iterate over checkpoints subtracting 5 from the platform.position.x

--- fcc-JSADS-BETA-OOP-Platformer-Game/script.js
+++ fcc-JSADS-BETA-OOP-Platformer-Game/script.js
@@ -131,6 +131,7 @@ const animate = () => {
         player.velocity.x = 0;
         if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
             platforms.forEach((platform) => platform.position.x -= 5);
+            checkpoints.forEach((checkpoint) => {checkpoint.position.x -= 5});
         } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
             platforms.forEach(platform => platform.position.x += 5);
         }
