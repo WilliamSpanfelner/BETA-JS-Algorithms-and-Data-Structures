91e89ca828f57dd4455768c9e8c2e8dbb2e1e53d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-03 12:53:43 +0300

    Access total-score & score-history elements

--- fcc-JSADS-BETA-Dice-Game/script.js
+++ fcc-JSADS-BETA-Dice-Game/script.js
@@ -4,3 +4,6 @@ const scoreSpans = document.querySelectorAll("#score-options span");
 
 const currentRoundText = document.getElementById("current-round");
 const currentRoundRollsText = document.getElementById("current-round-rolls");
+
+const totalScoreText = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
