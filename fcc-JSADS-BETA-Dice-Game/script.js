4e28be3b0d48ded170c1934f436b07d6f5d7d16c
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-04 16:06:35 +0300

    Set values if there was a selectedValue

--- fcc-JSADS-BETA-Dice-Game/script.js
+++ fcc-JSADS-BETA-Dice-Game/script.js
@@ -120,4 +120,11 @@ keepScoreBtn.addEventListener('click', () => {
             break;
         }
     };
+    if (selectedValue) {
+        rolls = 0;
+        round++;
+        updateStats();
+        resetRadioOption();
+        updateScore(selectedValue, achieved);
+    }
 });
