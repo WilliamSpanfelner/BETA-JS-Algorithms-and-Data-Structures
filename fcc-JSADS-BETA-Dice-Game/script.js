1f025ac7bd8c327cfc0430c6ba725b436df76055
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-05 14:51:26 +0300

    Create a set from the sortedNumbersArr

--- fcc-JSADS-BETA-Dice-Game/script.js
+++ fcc-JSADS-BETA-Dice-Game/script.js
@@ -102,6 +102,7 @@ const detectFullHouse = (arr) => {
 
 const checkForStraights = (arr) => {
     const sortedNumbersArr = arr.sort((a, b) => a - b);
+    const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
 };
 
 rollDiceBtn.addEventListener("click", () => {
