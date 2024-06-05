7d8a978ff36a552f065018cd130ebceb51cc2b3e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-05 13:57:03 +0300

    Call resetGame from keepScoreBtn

--- fcc-JSADS-BETA-Dice-Game/script.js
+++ fcc-JSADS-BETA-Dice-Game/script.js
@@ -144,8 +144,10 @@ keepScoreBtn.addEventListener('click', () => {
         updateScore(selectedValue, achieved);
         if (round > 6) {
             setTimeout(() => {
-                alert(`Game Over! Your total score is ${totalScore}`)
+                alert(`Game Over! Your total score is ${totalScore}`);
+                resetGame();
             }, 500);
+            
         }
     } else {
         alert("Please select an option or roll the dice");
