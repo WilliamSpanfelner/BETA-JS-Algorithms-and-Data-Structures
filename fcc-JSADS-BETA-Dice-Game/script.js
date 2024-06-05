55709a1af53b91cdbffe02149e8c67ba29770a18
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-05 15:09:32 +0300

    Create a string from uniqueNumbersArr

--- fcc-JSADS-BETA-Dice-Game/script.js
+++ fcc-JSADS-BETA-Dice-Game/script.js
@@ -103,6 +103,7 @@ const detectFullHouse = (arr) => {
 const checkForStraights = (arr) => {
     const sortedNumbersArr = arr.sort((a, b) => a - b);
     const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+    const uniqueNumbersStr = uniqueNumbersArr.join('');
 };
 
 rollDiceBtn.addEventListener("click", () => {
