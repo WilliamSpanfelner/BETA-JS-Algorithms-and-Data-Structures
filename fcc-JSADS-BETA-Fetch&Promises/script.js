47cf0fdafff4d00727a00fee4fff26eca08cb691
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-06 14:36:07 +0300

    Disable btn & Change btn text

--- fcc-JSADS-BETA-Fetch&Promises/script.js
+++ fcc-JSADS-BETA-Fetch&Promises/script.js
@@ -19,7 +19,10 @@ const fetchMoreAuthors = () => {
     startingIndex += 8;
     endingIndex += 8;
     displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-    if (authorDataArr.length <= endingIndex) { }
+    if (authorDataArr.length <= endingIndex) { 
+        loadMoreBtn.disabled = true;
+        loadMoreBtn.textContent = "No more data to load";
+    }
 };
 
 const displayAuthors = (authors) => {
