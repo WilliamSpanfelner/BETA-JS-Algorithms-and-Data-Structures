0346d12fe512f5660f4926766b174cad6782cc2a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-06 15:23:47 +0300

    Get access to posts-container id

--- fcc-JSADS-BETA-Forum-Leaderboard/script.js
+++ fcc-JSADS-BETA-Forum-Leaderboard/script.js
@@ -3,3 +3,5 @@ const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
+const postsContainer = document.getElementById("posts-container");
