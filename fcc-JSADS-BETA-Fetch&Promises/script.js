82f3d606a3599540616ac75da141d5130ae381a5
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-06 13:32:37 +0300

    Assign data to authorDataArr in the fetch

--- fcc-JSADS-BETA-Fetch&Promises/script.js
+++ fcc-JSADS-BETA-Fetch&Promises/script.js
@@ -1,14 +1,14 @@
 const authorContainer = document.getElementById("author-container");
 const loadMoreBtn = document.getElementById("load-more-btn");
 
-fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
-.then((res)=>res.json())
-.then((data)=>{console.log(data);})
-.catch((err)=>{console.error(`There was an error: ${err}`);});
-
 let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
+
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+.then((res)=>res.json())
+.then((data)=>{authorDataArr = data;})
+.catch((err)=>{console.error(`There was an error: ${err}`);});
 
 const displayAuthors = (authors) => {
     authors.forEach(({author, image, url, bio}, index)=>{
