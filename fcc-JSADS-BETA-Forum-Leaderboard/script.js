8d5e48e9034b3891a84e2ff0edd730724a44aed6
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-07 10:28:34 +0300

    Start populating data in postsContainer

--- fcc-JSADS-BETA-Forum-Leaderboard/script.js
+++ fcc-JSADS-BETA-Forum-Leaderboard/script.js
@@ -24,4 +24,5 @@ fetchData();
 const showLatestPosts = (data) => {
     const {topic_list, users} = data;
     const {topics} = topic_list;
+    postsContainer.innerHTML = topics.map((item)=>{});
 };
