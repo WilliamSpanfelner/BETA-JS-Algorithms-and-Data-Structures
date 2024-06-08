64c9064c062ec145e56581475836cbfb0376e85a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-08 18:27:37 +0300

    Add empty else clause for case when id is not in allCategories

--- fcc-JSADS-BETA-Forum-Leaderboard/script.js
+++ fcc-JSADS-BETA-Forum-Leaderboard/script.js
@@ -21,7 +21,7 @@ const forumCategory = (id) => {
         const {className, category} = allCategories[id];
         selectedCategory.className = className;
         selectedCategory.category = category;
-    }
+    } else {}
 };
 
 const timeAgo = (time) => {
