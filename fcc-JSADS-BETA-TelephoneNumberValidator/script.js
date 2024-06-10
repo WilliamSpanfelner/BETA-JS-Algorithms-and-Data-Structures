624ae2d147d71a53f47d02b3c5bc1bf8e7e62dbd
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-10 20:13:56 +0300

    Get access to the results-div

--- fcc-JSADS-BETA-TelephoneNumberValidator/script.js
+++ fcc-JSADS-BETA-TelephoneNumberValidator/script.js
@@ -1,6 +1,8 @@
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
 const clearButton = document.getElementById("clear-btn");
+const results = document.getElementById("results-div");
+results.innerText = "Hello";
 
 checkButton.addEventListener("click", ()=>{
     if (userInput.value === '') {
