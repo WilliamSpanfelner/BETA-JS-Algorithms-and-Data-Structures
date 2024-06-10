4680e609cf5452fea27b39969f3a2fcc53c687ab
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-10 20:25:39 +0300

    Add function to verify telephone numbers

--- fcc-JSADS-BETA-TelephoneNumberValidator/script.js
+++ fcc-JSADS-BETA-TelephoneNumberValidator/script.js
@@ -2,7 +2,15 @@ const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
 const clearButton = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
-results.innerText = "Hello";
+
+function telephoneCheck(str) {
+    const regex = /^(1\s\d{3}|1\s\(\d{3}|1\(\d{3}|\(\d{3}|\d{3})(\)\s|\)|\s|-)?\d{3}(\s|-)?\d{4}$/g;  
+
+    if (str.includes('(') && str.includes(')') || !str.includes('(') && !str.includes(')')) {
+        return regex.test(str); 
+    }
+    return false;
+}
 
 checkButton.addEventListener("click", ()=>{
     if (userInput.value === '') {
