b4713418c2bbdb6e9a882c7c2a24d3ec5c8a513e
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-10 19:59:52 +0300

    Alert if input empty and check-btn clicked

--- fcc-JSADS-BETA-TelephoneNumberValidator/script.js
+++ fcc-JSADS-BETA-TelephoneNumberValidator/script.js
@@ -1,4 +1,9 @@
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
 
-checkButton.addEventListener("click", ()=>{});
+checkButton.addEventListener("click", ()=>{
+    if (userInput.value === '') {
+        console.log("Empty input");
+        alert("Please provide a phone number");
+    }
+});
