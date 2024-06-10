9f7cc88f8c39a748fcbe6fc20582abeb3cd82e14
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-10 20:02:11 +0300

    Get access to the clear-btn

--- fcc-JSADS-BETA-TelephoneNumberValidator/script.js
+++ fcc-JSADS-BETA-TelephoneNumberValidator/script.js
@@ -1,5 +1,6 @@
 const userInput = document.getElementById("user-input");
 const checkButton = document.getElementById("check-btn");
+const clearButton = document.getElementById("clear-btn");
 
 checkButton.addEventListener("click", ()=>{
     if (userInput.value === '') {
