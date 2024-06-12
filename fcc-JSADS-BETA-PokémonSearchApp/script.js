2809fdc1edd2195e110d3797e0af7c4728eda03a
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-12 19:55:14 +0300

    Get access to div elemtents

--- fcc-JSADS-BETA-PokémonSearchApp/script.js
+++ fcc-JSADS-BETA-PokémonSearchApp/script.js
@@ -1,6 +1,17 @@
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonHP = document.getElementById("hp");
+const pokemonAttack = document.getElementById("attack");
+const pokemonDefense = document.getElementById("defense");
+const pokemonSpecialAttack = document.getElementById("special-attack");
+const pokemonSpecialDefense = document.getElementById("special-defense");
+const pokemonSpeed = document.getElementById("speed");
+
 
 searchButton.addEventListener("click", ()=>{
     if (searchInput.value === "Red") {
