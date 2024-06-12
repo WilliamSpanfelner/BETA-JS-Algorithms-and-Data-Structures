01ab0b1b410e0fd5179c52e2f6d4f02faa7a7323
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-06-12 19:48:56 +0300

    Create callback for input values

--- fcc-JSADS-BETA-PokémonSearchApp/script.js
+++ fcc-JSADS-BETA-PokémonSearchApp/script.js
@@ -1,4 +1,9 @@
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
-searchButton.addEventListener("click", ()=>{});
+
+searchButton.addEventListener("click", ()=>{
+    if (searchInput.value === "Red") {
+        alert("Pokémon not found");
+    }
+});
